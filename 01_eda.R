3de59a68f6c80bc94289d65ae3cd32dec6705e38
Derek Weix <47987393+dpweix@users.noreply.github.com>
2026-04-15 12:50:14 -0400

    Read, clean, and write penguins data

--- 01_eda.R
+++ 01_eda.R
@@ -1,2 +1,20 @@
 library(tidyverse)
 set_theme(theme_bw())
+
+# Load data --------------------------------------------------------------
+df <- read_csv("data/penguins.csv")
+
+# View variables
+glimpse(df)
+
+# Check count of NA observations
+df |>
+  summarize(across(everything(), \(x) sum(is.na(x))))
+
+# Remove NA observations
+df_clean <- df |>
+  filter_out(if_any(everything(), is.na))
+
+# Save clean data
+write_csv(df_clean, "data/penguins_clean.csv")
+
