83b4dd165a68515e98ddfe0c6b030a53336c635a
Derek Weix <47987393+dpweix@users.noreply.github.com>
2026-04-15 12:51:51 -0400

    Generate and save EDA plots

--- 01_eda.R
+++ 01_eda.R
@@ -18,3 +18,22 @@ df_clean <- df |>
 # Save clean data
 write_csv(df_clean, "data/penguins_clean.csv")
 
+
+# Generate plots ---------------------------------------------------------
+
+# Scatter plot
+df_clean |>
+  ggplot(aes(body_mass, flipper_len, color = sex)) +
+  geom_point(shape = 21) +
+  facet_wrap(~species)
+
+ggsave("figures/scatterplot.png", width = 1700, height = 1000, units = "px")
+
+# Histogram
+df_clean |>
+  pivot_longer(c(body_mass, flipper_len)) |>
+  ggplot(aes(value, fill = sex)) +
+  geom_histogram() +
+  facet_grid(species ~ name, scales = "free")
+
+ggsave("figures/histogram.png", width = 1500, height = 1000, units = "px")
